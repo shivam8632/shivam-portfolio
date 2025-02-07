7bf94ebef0651a2423db2f68acc7ba5191b89528
shivamRocketPOS <shivam@rocketPOS.co.nz>
2025-02-07 12:45:38 +0530

    First commit

---
+++ src/assets/index.js
@@ -0,0 +1,92 @@
+// Import company logos
+import mathwork from "./company/mathwork.png";
+import edunet from "./company/edunet.png";
+import eduskill from "./company/eduskill.png";
+import tka from "./company/tka.jpg";
+import codenomad from "./company/codenomad.png";
+import expinator from "./company/expinator.png";
+import rocketpos from "./company/rocketpos.png";
+
+// Import tech stack images
+import python from "./tech-stack/python.png";
+import c from "./tech-stack/c.png";
+import cpp from "./tech-stack/cpp.png";
+import java from "./tech-stack/java.png";
+
+// Import project images
+import weatherpedia from "./projects/WeatherPedia.png";
+import termpw from "./projects/termpw.png";
+import payloadmaster from "./projects/payloadmaster.png";
+import mhft from "./projects/mhft.png";
+import sketcher from "./projects/sketcher.png";
+import CompileVortex from "./projects/CompileVortex.png";
+import bestpractice from "./projects/bestpractice.png";
+import brainly from "./projects/brainly.png";
+import cheveux from "./projects/cheveux.png";
+import meeran from "./projects/meeran.png";
+import hostbuddy from "./projects/host-buddy.png";
+import skeww from "./projects/skeww.png";
+
+// Import tech logos
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.png";
+import scss from "./tech/scss.svg";
+import less from "./tech/less.png";
+import bootstrap from "./tech/bootstrap.png";
+import jquery from "./tech/jquery.ico";
+import typescript from "./tech/typescript.png";
+
+// Import common assets
+import logo from "./logo.svg";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export {
+  mathwork,
+  edunet,
+  python,
+  c,
+  cpp,
+  java,
+  github,
+  css,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  weatherpedia,
+  termpw,
+  payloadmaster,
+  mhft,
+  sketcher,
+  CompileVortex,
+  logo,
+  menu,
+  close,
+  eduskill,
+  scss,
+  less,
+  bootstrap,
+  typescript,
+  jquery,
+  tka,
+  codenomad,
+  expinator,
+  rocketpos,
+  bestpractice,
+  brainly,
+  cheveux,
+  hostbuddy,
+  skeww,
+  meeran,
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,70 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { Tilt } from "react-tilt";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+          loading="lazy"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a passionate Frontend Developer skilled in HTML, CSS, SCSS/LESS,
+        Bootstrap, Tailwind, JavaScript, jQuery, TypeScript, React.js, and
+        Shopify. I specialize in building responsive, high-performance websites
+        with clean code and modern UI/UX practices. From pixel-perfect designs
+        to eCommerce solutions, I create engaging digital experiences. Always
+        eager to learn, I stay updated with the latest web trends to deliver
+        innovative solutions. 🚀
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const WrappedAbout = SectionWrapper(About, "about");
+
+export default WrappedAbout;
